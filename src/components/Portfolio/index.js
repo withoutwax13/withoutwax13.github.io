3236ee3f58dcf993ed96f84ceba0c1cf635add9d
John Patrick Valera <johnpatrickyusoresvalera@gmail.com>
2023-01-09 04:31:44 +0800

    updated portfolio page for nondesktop devices

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -110,7 +110,62 @@ function Desktop() {
 }
 
 const NonDesktop = () => {
-    return <></>
+    return (
+        <Grid
+            templateColumns='repeat(1, 1fr)' gap={20}
+            rowGap={20} 
+            style={{
+                zIndex: '5',
+                position: 'absolute',
+                color: 'white',
+                marginTop: "123px",
+                paddingLeft: '5%',
+                paddingRight: '5%',
+                paddingTop: "0",
+                paddingBottom: "2%"}}>
+                <GridItem>
+                    <ProjectCard
+                            title={`Mealoo`}
+                            desc={`Mealoo provides an alternative to directly hiring professional nutritionists and dietitians. It has a monthly or annually subscription business model.`}
+                            repoLink={`https://github.com/withoutwax13/mealoo-static`}
+                            projectLink={`https://withoutwax13.github.io/mealoo-static/`}
+                    />
+                </GridItem>
+                <GridItem w='100%'>
+                    <ProjectCard 
+                        title={`PushCart.Com (E-commerce Web App)`}
+                        projectLink={`https://withoutwax13.github.io/pushcart/`}
+                        desc={`Client web application built on ReactJS that
+                        serves as the buyer portal for PushCart.Com's
+                        series of applications built as a capstone
+                        requirement for a BootCamp. Other app inclusions: Seller portal, Express and
+                        laravel backend`}
+                        repoLink={`https://github.com/withoutwax13/pushcart`}
+                    />
+                </GridItem>
+                <GridItem>
+                    <ProjectCard
+                            title={`COVID-19 TRACKER (Real-time Data Tracker Web App)`}
+                            desc={`Client web application built on ReactJS that
+                            shows real-time data of COVID-19 cases, deaths
+                            Utilized react-globe and recharts for visualizing
+                            data per country, per data range
+                            `}
+                            repoLink={`https://github.com/withoutwax13/track-covid19`}
+                            projectLink={`http://withoutwax13.github.io/track-covid19`}
+                    />
+                </GridItem>
+                <GridItem>
+                    <ProjectCard
+                            title={`Group-Work`}
+                            desc={`Social Media Service 
+                            catering group discussions/projects`}
+                            repoLink={`https://github.com/withoutwax13/group-work`}
+                            projectLink={`https://github.com/withoutwax13/group-work`}
+                    />
+                </GridItem>
+        </Grid>
+    );
 }
 
 const Portfolio = () => {
