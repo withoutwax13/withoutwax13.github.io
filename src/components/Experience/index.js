577a7057919dc4cd30acaf9414e54b0c0d07f15e
John Patrick Valera <johnpatrickyusoresvalera@gmail.com>
2023-01-09 04:29:41 +0800

    updated experience page for nondesktop devices

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -2,7 +2,7 @@ import { Button, Heading, VStack, Grid, GridItem, Highlight} from '@chakra-ui/re
 import { useState } from 'react';
 
 
-const experienceText = [
+const experienceTextForDesktop = [
     <Heading as="h3" fontSize="40px" fontFamily="Calibri" style={{color: "white", paddingTop: '30px', paddingRight: '30px'}}>
         <Highlight
             query={['917Venture/KodeGo Fullstack PHP Bootcamp', 'Upskilled', 'expanded network']}
@@ -46,6 +46,52 @@ const experienceText = [
         
     </Heading>,
 ]
+
+const experienceTextForNonDesktop = [
+    <Heading as="h3" fontSize="12px" fontFamily="Calibri" style={{color: "white", paddingTop: '30px', paddingRight: '30px'}}>
+        <Highlight
+            query={['917Venture/KodeGo Fullstack PHP Bootcamp', 'Upskilled', 'expanded network']}
+            styles={{ px: '2', py: '1', rounded: 'full', bg: 'teal.100' }}
+        >
+            Upskilled and expanded network by graduating on 917Venture/KodeGo Fullstack PHP Bootcamp program.
+        </Highlight>
+    </Heading>,
+    <Heading as="h3" fontSize="12px" fontFamily="Calibri" style={{color: "white", paddingTop: '30px', paddingRight: '30px'}}>
+        <Highlight
+            query={['BS Mechanical Engineering']}
+            styles={{ px: '2', py: '1', rounded: 'full', bg: 'teal.100' }}
+        >
+            Graduated on Bicol State College of Applied Sciences and Technology with a degree on BS Mechanical Engineering.
+        </Highlight>
+    </Heading>,
+    <Heading as="h3" fontSize="12px" fontFamily="Calibri" style={{color: "white", paddingTop: '30px', paddingRight: '30px'}}>
+        <Highlight
+            query={['focus on freelance business', 'Quantrics Enterprise, Inc.']}
+            styles={{ px: '2', py: '1', rounded: 'full', bg: 'teal.100' }}
+        >
+            Parted ways with Quantrics Enterprise, Inc. to focus on freelance business.
+        </Highlight>
+        
+    </Heading>,
+    <Heading as="h3" fontSize="12px" fontFamily="Calibri" style={{color: "white", paddingTop: '30px', paddingRight: '30px'}}>
+        <Highlight
+            query={['Quantrics Enterprise, Inc.']}
+            styles={{ px: '2', py: '1', rounded: 'full', bg: 'teal.100' }}
+        >
+            Joined Quantrics Enterprise, Inc. as a Customer Service Representative.
+        </Highlight>
+    </Heading>,
+    <Heading as="h3" fontSize="12px" fontFamily="Calibri" style={{color: "white", paddingTop: '30px', paddingRight: '30px'}}>
+        <Highlight
+            query={['software development services']}
+            styles={{ px: '2', py: '1', rounded: 'full', bg: 'teal.100' }}
+        >
+            Started freelance business providing software development services.
+        </Highlight>
+        
+    </Heading>,
+]
+
 function Desktop() {
     const [activeNavIndex, setActiveNavIndex] = useState(0)
     return (
@@ -77,14 +123,47 @@ function Desktop() {
                 colSpan={3}
                 rowSpan={2}
             >
-                {experienceText[activeNavIndex]}
+                {experienceTextForDesktop[activeNavIndex]}
             </GridItem>
         </Grid>
     );
 }
 
 const NonDesktop = () => {
-    return <>Experience</>
+    const [activeNavIndex, setActiveNavIndex] = useState(0)
+    return (
+        <Grid 
+            style={{
+                zIndex: '5',
+                position: 'absolute',
+                color: 'white',
+                marginTop: "123px",
+                marginLeft: "10px",
+                paddingTop: "0",
+            }}
+            templateColumns='repeat(4,1fr)'
+            columnGap={30}
+          >
+            <GridItem
+                colSpan={1}
+                rowSpan={3}
+            >
+                <VStack direction='column' align='center'>
+                    <Button as="p" onClick={()=>setActiveNavIndex(0)} fontSize="20px" fontFamily="Calibri" style={{color: `${activeNavIndex === 0 ? 'white' : '#AE8898'}`, cursor: 'pointer', margin: '0.5rem'}}>Nov 2022</Button>
+                    <Button as="p" onClick={()=>setActiveNavIndex(1)} fontSize="20px" fontFamily="Calibri" style={{color: `${activeNavIndex === 1 ? 'white' : '#AE8898'}`, cursor: 'pointer', margin: '0.5rem'}}>Apr 2022</Button>
+                    <Button as="p" onClick={()=>setActiveNavIndex(2)} fontSize="20px" fontFamily="Calibri" style={{color: `${activeNavIndex === 2 ? 'white' : '#AE8898'}`, cursor: 'pointer', margin: '0.5rem'}}>Jul 2022</Button>
+                    <Button as="p" onClick={()=>setActiveNavIndex(3)} fontSize="20px" fontFamily="Calibri" style={{color: `${activeNavIndex === 3 ? 'white' : '#AE8898'}`, cursor: 'pointer', margin: '0.5rem'}}>Oct 2021</Button>
+                    <Button as="p" onClick={()=>setActiveNavIndex(4)} fontSize="20px" fontFamily="Calibri" style={{color: `${activeNavIndex === 4 ? 'white' : '#AE8898'}`, cursor: 'pointer', margin: '0.5rem'}}>Jan 2020</Button>
+                </VStack>
+            </GridItem>
+            <GridItem
+                colSpan={3}
+                rowSpan={2}
+            >
+                {experienceTextForNonDesktop[activeNavIndex]}
+            </GridItem>
+        </Grid>
+    );
 }
 
 const Experience = () => {
